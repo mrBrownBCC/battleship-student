fe7689a7ce2ca411c444b889569ec4f385838055
Everett Brown <everett_a_brown2@mcpsmd.org>
2025-03-06 03:39:55 +0000

    at least working

--- battleship/app/src/test/java/bcc/battleship/Freebie.java
+++ battleship/app/src/test/java/bcc/battleship/Freebie.java
@@ -1,5 +1,10 @@
 package bcc.battleship;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class Freebie {
-    
+    @Test
+    public void alwaysPasses() {
+        assertTrue("This test should always pass!", true);
+    }
 }
